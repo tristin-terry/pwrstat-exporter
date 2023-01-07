e96ed9f3de050d03f7331e18683e51fbafa3af56
Tristin Terry <tristin.sean.terry@gmail.com>
2023-01-07 10:01:16 -0600

    add renovate

---
+++ renovate-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    branchPrefix: 'renovate/',
+    dryRun: true,
+    username: 'renovate-release',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: false,
+    platform: 'github',
+    includeForks: true,
+    repositories: [
+      'tristin-terry/pwrstat-exporter'
+    ]
+  };
