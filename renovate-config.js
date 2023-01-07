0cfe270acae1b3f2c959f693ed4be85d478a2cdc
Tristin Terry <tristin.sean.terry@gmail.com>
2023-01-07 11:35:09 -0600

    renovate onboarding

--- renovate-config.js
+++ renovate-config.js
@@ -2,7 +2,6 @@ module.exports = {
     branchPrefix: 'renovate/',
     username: 'renovate-release',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    onboarding: false,
     platform: 'github',
     includeForks: true,
     repositories: [
