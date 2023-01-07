5b20aa9fc609cdef10647e5196026e07f26d7cca
Tristin Terry <tristin.sean.terry@gmail.com>
2023-01-07 11:16:13 -0600

    fix renovate dryrun config

--- renovate-config.js
+++ renovate-config.js
@@ -1,6 +1,5 @@
 module.exports = {
     branchPrefix: 'renovate/',
-    dryRun: true,
     username: 'renovate-release',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     onboarding: false,
